802eb96b93320d82ce1030c69fcd5cfc07c3d8bc
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-31 07:09:27 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+
+public class Main {
+
+/*
+    The main	method creates	an	instance	of	the
+    class, which displays a	window with	a slider.
+*/
+
+public static void main(String[]	args)
+{
+    new TempConverter();
+}
+}
